5c6522efbc04c4e20592711ee7c079c4686ec6d8
Blackagent <ja7n7ne@gmx.de>
2015-04-02 21:34:43 +0200

    In Bearbeitung

--- src/net/blockbreaker/mysqlmanagement/MySQLMethods.java
+++ src/net/blockbreaker/mysqlmanagement/MySQLMethods.java
@@ -14,7 +14,7 @@ import java.util.Locale;
 public class MySQLMethods {
 
     public static void createTableIfNotExists() {
-        MySQL.update("CREATE TABLE IF NOT EXISTS data (playername VARCHAR(100), uuid VARCHAR(100), nick BOOLEAN, lastLoginAsString VARCHAR(100))");
+        MySQL.update("CREATE TABLE IF NOT EXISTS data (playername VARCHAR(100), uuid VARCHAR(100), lastlogin VARCHAR(100), nick BOOLEAN)");
 
         MySQL.update("CREATE TABLE IF NOT EXISTS rpg (playername VARCHAR(100), uuid VARCHAR(100), ep INTEGER, coins INTEGER, campaignprogress INTEGER))");
     }
@@ -38,7 +38,7 @@ public class MySQLMethods {
 
             try {
                 if (!rs.next()) {
-                    MySQL.update("INSERT INTO data VALUES('" + player.getName() + "', '" + uuid + "', '" + nick + "', '" + lastLoginAsString + "')");
+                    MySQL.update("INSERT INTO data VALUES('" + player.getName() + "', '" + uuid + "', '" + lastLoginAsString + "', '" + nick + "')");
                     return;
                 }
             } catch (SQLException e) {
