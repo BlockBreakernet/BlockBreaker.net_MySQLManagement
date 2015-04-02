220f8123760c7d1a33b9badba13492e18c8b4710
Blackagent <ja7n7ne@gmx.de>
2015-04-02 21:54:36 +0200

    In Bearbeitung

--- src/net/blockbreaker/mysqlmanagement/MySQLMethods.java
+++ src/net/blockbreaker/mysqlmanagement/MySQLMethods.java
@@ -53,7 +53,7 @@ public class MySQLMethods {
 
             try {
                 if (!sr.next()) {
-                    MySQL.update("INSERT INTO rpg VALUES('" + player.getName() + "', '" + uuid + "', '" + ep + "', '" + coins + "', '" + campaignprogress + "')");
+                    MySQL.update("INSERT INTO rpg VALUES('" + player.getName() + "', '" + uuid + "', ep, coins, campaignprogress)");
                     return;
                 }
             } catch (SQLException e) {
